c245bcb76cd13ecf3398700c490b7da390ed63ed
quatermain <quatermain32@gmail.com>
2012-04-19 10:36:31 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoRailsonfire::Application.config.secret_token = '6c54044601a33977a7376e58f49d5855d54843967663ca137f74070a8655dface39e0f82651ffca117aadc8e50ca60a0bed041c930a5643da4869cd72c41659f'
